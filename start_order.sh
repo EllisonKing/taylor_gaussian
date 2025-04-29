e298e7129f5f4837397fa42a97404f1efd0183f5
Ellison6688 <167402033+EllisonKing@users.noreply.github.com>
2025-04-29 20:24:12 +0800

    Update start_order.sh

--- start_order.sh
+++ start_order.sh
@@ -3,13 +3,12 @@ GPU_ID=$1
 
 
 run_pipeline() {
-
     # train
     CUDA_VISIBLE_DEVICES="$GPU_ID" python main.py \
     --source_path data/Neural3D/"$data_name"/colmap_0 \
     --model_path logs/N3DV/"$data_name"_test \
     --deform_type node \
-    --node_num 4096 \
+    --node_num 1024 \
     --hyper_dim 8 \
     --is_blender \
     --gt_alpha_mask_as_dynamic_mask \
@@ -20,16 +19,12 @@ run_pipeline() {
     --W 800 \
     --H 800 \
     --config arguments/N3DV/"$data_name".py
-    
-
-
-
     # render
     CUDA_VISIBLE_DEVICES="$GPU_ID" python render.py \
     --source_path data/Neural3D/"$data_name"/colmap_0 \
     --model_path logs/N3DV/"$data_name"_test \
     --deform_type node \
-    --node_num 4096 \
+    --node_num 1024 \
     --hyper_dim 8 \
     --is_blender \
     --gt_alpha_mask_as_dynamic_mask \
@@ -42,13 +37,11 @@ run_pipeline() {
     --dataset_type colmap
 }
 
-
-Neural3D_DA=("cut_roasted_beef" "sear_steak" "flame_steak" "cook_spinach" "flame_salmon_1" "coffee_martini" )
-
+Neural3D_DA=("cut_roasted_beef" "sear_steak" "flame_steak")
+Neural3D_DA=( "cook_spinach" "flame_salmon_1" "coffee_martini" )
 # run_pipeline
 for data_name in "${Neural3D_DA[@]}"; 
 do
     echo "Dataset: Neural3D_DA/${data_name}"
     run_pipeline "$GPU_ID" "$data_name"
 done
-
