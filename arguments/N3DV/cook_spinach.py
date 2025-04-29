9c6f7cea0fc469e6a5a3b0fef2c1afac980ccca2
Ellison6688 <167402033+EllisonKing@users.noreply.github.com>
2025-04-29 10:15:15 +0800

    Update cook_spinach.py

--- arguments/N3DV/cook_spinach.py
+++ arguments/N3DV/cook_spinach.py
@@ -9,7 +9,7 @@ OptimizationParams = dict(
 #    position_lr_init=1.6e-5/10, #1.6e-5 # 0.00016/5,
 
     densify_grad_threshold=0.0002, # 0.0002
-    scaling_lr=0.005, # 0.001
+    # scaling_lr=0.005, # 0.001
     position_lr_init=1.6e-4, #1.6e-5 # 0.00016/5,
     flag=0
 )
