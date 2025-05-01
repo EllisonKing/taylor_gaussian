c94628c41aa487c6aa8dcb0b31ca01f83f93e269
Ellison6688 <167402033+EllisonKing@users.noreply.github.com>
2025-05-01 11:20:53 +0800

    Update coffee_martini.py

--- arguments/N3DV/coffee_martini.py
+++ arguments/N3DV/coffee_martini.py
@@ -1,6 +1,12 @@
 ModelParams = dict(
     dataset_type=None,  # N3DV
-
+    # densify_grad_threshold = 0.0002, # 0.0002
+    #scaling_lr = 0.003, # 0.001
+    # position_lr_init = 0.00016/5, #1.6e-5, #0.00016/5,#0.00016/5, #1.6e-5 #0.00016,
+    # rotation_lr = 1e-4, #0.001,
+    # movelr = 3.5/14,
+    position_lr_init = 0.00016,
+    flag=0
 )
 
 OptimizationParams = dict(
